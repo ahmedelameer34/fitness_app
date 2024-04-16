43b03f51b9872aa7869d464c02c7e7b694d84cbf
Ahmed Badawy Mohamed El Amir <amirbadawy68@gmail.com>
2024-04-16 07:13:57 +0200

    about screen ui

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,22 +1,23 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:fittness_app/pages/about_view.dart';
 import 'package:fittness_app/pages/welcome_view.dart';
 import 'package:get/get.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const initial = Routes.welcome;
+  static const initial = Routes.about;
 
   static final routes = [
     GetPage(
       name: Routes.welcome,
       page: () => const WelcomeView(),
     ),
-    // GetPage(
-    //   name: Routes.ABOUT,
-    //   page: () => const AboutView(),
-    // ),
+    GetPage(
+      name: Routes.about,
+      page: () => const AboutView(),
+    ),
     // GetPage(
     //   name: Routes.LOGIN,
     //   page: () => const LoginView(),
