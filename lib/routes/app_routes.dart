11998362efc911a8798189ce23aab9bdb4c4542e
Ahmed Badawy Mohamed El Amir <amirbadawy68@gmail.com>
2024-04-17 07:08:42 +0200

    register screen ui

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -8,4 +8,5 @@ abstract class Routes {
   static const about = '/about-you';
   static const login = '/login';
   static const forgetPassword = '/forget-password';
+  static const register = '/register';
 }
